15b6c4e69f46679d654e9e7129bfa91ea244c60b
Saeed Ganji <shahabganji@gmail.com>
2021-10-11 21:24:02 +0200

    init

---
+++ Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using LibGit2Sharp;
+using static System.Console;
+using static System.String;
+
+namespace DotnetGlobalToolGitClearBranches
+{
+    internal static class Program
+    {
+        private static Task<int> Main(string[] args)
+        {
+            var repositoryPath = ResolveRepositoryPath();
+            if (IsNullOrEmpty(repositoryPath))
+            {
+                WriteLine("fatal: Not a git repository");
+                return Task.FromResult(-1);
+            }
+
+            var pathToGit = ResolveGitPath();
+            if (IsNullOrEmpty(pathToGit))
+            {
+                WriteLine("fatal: git is not in your path");
+                return Task.FromResult(-2);
+            }
+            
+            /**
+             * git branch --merged | %{$_.trim()}  | ?{$_ -notmatch 'dev' -and $_ -notmatch 'master'} | %{git branch -d $_}
+             */
+
+
+            var path = repositoryPath.Substring(0, repositoryPath.LastIndexOf("/.git/", StringComparison.Ordinal));
+            WriteLine(path);
+            var repository = new Repository(path);
+
+            var notTrackingBranches = repository.Branches.ToList();
+            WriteLine($"Number of non-tracking branches {notTrackingBranches.Count()}");
+            foreach (var branch in notTrackingBranches)
+            {
+                WriteLine(branch.FriendlyName);
+            }
+            
+            return Task.FromResult(0);
+        }
+
+        private static string ResolveRepositoryPath()
+        {
+            var currentDirectory = Directory.GetCurrentDirectory();
+            var repositoryPath = Repository.Discover(currentDirectory);
+
+            if (IsNullOrEmpty(repositoryPath))
+                return Empty;
+
+            return Repository.IsValid(repositoryPath) 
+                ? repositoryPath 
+                : Empty;
+        }
+
+        private static string ResolveGitPath()
+        {
+            var path = Environment.GetEnvironmentVariable("PATH");
+            if (IsNullOrEmpty(path))
+                return Empty;
+
+            var paths = path.Split(Path.PathSeparator);
+            
+            var fileNames = new[] { "git.exe", "git" };
+            var searchPaths = paths.SelectMany(p => fileNames.Select(f => Path.Combine(p, f)));
+
+            return searchPaths.FirstOrDefault(File.Exists) ?? Empty;
+        }
+    }
+}
